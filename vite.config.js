3fc1b6c2f5a9e8ad5e0a45245111659c04eb1263
dimaldo86 <dimaldo86ne@mail.ru>
2024-03-28 18:08:30 +0700

    finished test task

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `@import "@/scss/variables.scss";`,
+        additionalData: `@import "./src/scss/vars.scss";`,
       },
     },
   }
